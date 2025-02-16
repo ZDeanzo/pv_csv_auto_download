70348237b42dcd9b3a3a6922d0959f2132def1e1
ZDeanzo <47153579+ZDeanzo@users.noreply.github.com>
2025-02-16 19:10:37 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
+#This script allows a quick capture of all of the raw solar readings from your Passiv Living account.
+# You will need a CSV with the dates for the year you wish to download. I have included many for ease of use.
+# There no doubt is a more effecive way but this works sufficently well for my needs.
+
 
 echo "Please specify year you wish to download"
 read year
-
 
 #### REPLACE URL WITH YOUR ACCOUNT & TOKEN INFORMATION ####
 
 start="https://passivliving.com/lpa/v/1/homes/XXXXX/solar/generation/graph/"
 end="/graph.csv?overlay=None&language=en&isLocaltime=true&access_token=XXXXXXXXXXXXXXXXXXXXXXXXX"
-
-
 
 #Check and create year directory if needed
 if [ -d ./$year ]; then
